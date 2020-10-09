2a1b73b1b1fa82e3ad1161ed58c273dda98417d1
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-10-09 18:22:15 -0400

    Project Template

---
+++ JabariBears/JabariBears/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JabariBears
+//
+//  Created by Subomi Popoola on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JabariBears/JabariBears/JabariBearsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JabariBearsApp.swift
+//  JabariBears
+//
+//  Created by Subomi Popoola on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct JabariBearsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
